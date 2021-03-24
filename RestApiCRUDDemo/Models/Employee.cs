9d82a3e8dfaea96a0b2424830ba3b3166de81eff
ravi.kuntal1 <dell@DESKTOP-70SDBF5>
2021-03-24 14:48:56 +0530

    Add project files.

---
+++ RestApiCRUDDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUDDemo.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+ 
+    }
+}
